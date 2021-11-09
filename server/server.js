f7ff73b8b7f7d23b1f50f64549bf8b31cd7d5338
Kelsey Brown <kbrown55347@gmail.com>
2021-11-09 17:03:15 -0600

    created server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+console.log('Look at me go!');
+
+// Load express library from node_modules/express:
+const express = require('express');
+
+// Create a variable called "app," OUR SERVER:
+const app = express();
+
+// Tell express where to find our "public" files.
+    // aka "the client-side files"
+    // aka "static assets"
+    // aka "what we've been doing already"
+app.use(express.static('./server/public'));
+
+
+// Starts server and listens for requests:
+// 500 is the port (personal note)
+app.listen(5000, function() {
+    console.log('Your first express server is running!');
+});
